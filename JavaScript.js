f58633ec84f3d2da077c73fc94aae684767da258
HurricaneHolden <100702336+RubyRain1@users.noreply.github.com>
2023-09-26 21:19:52 -0400

    java scrip shenanigans

---
+++ JavaScript.js
@@ -0,0 +1,20 @@
+console.log("does this work?")
+
+console.log("this is ever updated")
+
+let message;
+
+message = 'Hello WOrld this is let';
+
+console.log(message) /* this is how to print out to the console on the inspect element. */
+
+let age = 22, 
+user = "Ruby";
+
+console.log(age,user)
+
+var int = 1;
+
+console.log(int)
+
+console.log(1+2+44+38) /77; 
