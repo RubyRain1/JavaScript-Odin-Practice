9cd701d4bec65a4e72129253b9fb4404bf872963
Holden Hughes <100702336+RubyRain1@users.noreply.github.com>
2023-09-27 12:03:24 -0400

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -18,3 +18,26 @@ var int = 1;
 console.log(int)
 
 console.log(1+2+44+38) /77; 
+
+let test='hello this is me'
+
+console.log(test)
+
+let slice= test.slice(1,5); /* this actually has to be the name of the string */
+
+console.log(slice)
+
+let sub= test.substring(3,6);
+
+console.log(sub)
+
+let test2= "this is just a new string so I can test"
+let str= test2.substr(0,10);
+
+console.log(str);
+
+let rep = "this is to test school"
+let newText= rep.replace("school", "replace") /* first word is the word wanting to replace, second is the word replacing. */
+
+console.log(rep, newText)
+
